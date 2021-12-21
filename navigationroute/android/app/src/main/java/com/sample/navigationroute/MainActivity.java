69eb5e90327ba624dba0f8b5aa142416eb4fe902
sree2694 <58017164+sree2694@users.noreply.github.com>
2021-12-21 10:22:19 +0530

    Navigation route

---
+++ navigationroute/android/app/src/main/java/com/sample/navigationroute/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sample.navigationroute;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ navigationroute/lib/nav_route.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class navroute extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+  final routes = <String, WidgetBuilder> {
+    SecondScreen.tag:(context) => SecondScreen(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Routes",
+      home: new FirstScreen(),
+      routes: routes,
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("First Widget"),
+      ),
+      body: RaisedButton(onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+//        Navigator.of(context).pushNamed(SecondScreen.tag);
+      },
+        child: Text("Go to second screen"),),
+    );
+  }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  static String tag = "second_widget";
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Second Widget"),
+      ),
+      body: RaisedButton(onPressed: () {
+        Navigator.pop(context);
+      },
+        child: Text("Go Back"),),
+    );
+  }
+}
